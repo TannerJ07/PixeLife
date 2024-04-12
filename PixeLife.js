cbf2726e906fb9328dc828b1b62bcd8c0b48d66c
TannerJ07 <jenkinstj256@gmail.com>
2024-04-12 10:01:43 -0500

    Added Selection Panel

--- PixeLife.js
+++ PixeLife.js
@@ -1,5 +1,5 @@
 const boardWidth = 50
-const boardHeight = boardWidth
+const boardHeight = 50
 
 const cellSize = 12
 const canvasSize = boardWidth*cellSize
@@ -8,15 +8,20 @@ let grid = {}
 let newGrid = {};
 let context;
 let board;
+let selector
 let mousedown = false;
 let mousex, mousey;
-let element = "sand";
-
+let element = "water";
+let catagory = "solid";
 const colors = {
-    air: "rgb(200,200,200)",
-    sand: "yellow",
-    water: "blue",
-    border: "palevioletred",
+    air: [200,200,200],
+    sand: [255,230,179],
+    water: [0,0,255],
+    border: [255,0,255],
+
+
+    solid: [70,70,70],
+    liquid: [0,0,255],
 };
 const functions = {
     air: null,
@@ -24,6 +29,12 @@ const functions = {
     water: moveWater,
     border: null,
 }
+const elementCatagory = {
+    solid: ["sand"],
+    liquid: ["water"],
+}
+const catagories = ["solid","liquid",]
+
 function moveWater(x,y) {
     let direction = Math.floor(Math.random()*3)-1
     if (newGrid[x][y+1] == "air") {
@@ -50,6 +61,7 @@ window.onload = function() {
     board.height = canvasSize
     board.width = canvasSize
     context = board.getContext("2d")
+    selector = document.getElementById("selector")
     setGame();
     
     board.onmousedown = function(e) {
@@ -62,9 +74,9 @@ window.onload = function() {
         mousey = e.offsetY
     }
     board.onmouseup = function() {
-        mousedown = false
+        mousedown = null
     }
-    window.oncontextmenu = function () { return false; }
+    board.oncontextmenu = function () { return false; }
     setInterval(update,1000/60);
 }
 
@@ -115,13 +127,74 @@ function setGame (){
             grid[i][j] = "air";
         }
     }
+    let catagoryButton;
+    for (let i = 0;i<catagories.length;i++) {
+        catagoryButton =  document.createElement("button");
+        catagoryButton.appendChild(document.createTextNode(catagories[i].toUpperCase()));
+        let color = `rgb(
+            ${colors[catagories[i]][0]},
+            ${colors[catagories[i]][1]},
+            ${colors[catagories[i]][2]})`;
+        catagoryButton.style.color = color;
+        color = `rgba(
+            ${colors[catagories[i]][0]},
+            ${colors[catagories[i]][1]},
+            ${colors[catagories[i]][2]},
+            0.5)`
+        catagoryButton.style.backgroundColor = `${color}`;
+        catagoryButton.style.backgroundColor.opacity = 0.2;
+        catagoryButton.addEventListener("click",switchCatagory)
+        catagoryButton.setAttribute("id",catagories[i])
+        let button;
+        for (let j = 0; j<elementCatagory[catagories[i]].length;j++) {
+            
+            button =  document.createElement("button");
+            button.appendChild(document.createTextNode(elementCatagory[catagories[i]][j].toUpperCase()));
+            let color = `rgb(
+                ${colors[elementCatagory[catagories[i]][j]][0]},
+                ${colors[elementCatagory[catagories[i]][j]][1]},
+                ${colors[elementCatagory[catagories[i]][j]][2]})`;
+                button.style.color = color;
+            color = `rgba(
+                ${colors[elementCatagory[catagories[i]][j]][0]},
+                ${colors[elementCatagory[catagories[i]][j]][1]},
+                ${colors[elementCatagory[catagories[i]][j]][2]},
+                0.5)`
+            button.style.backgroundColor = `${color}`;
+            button.style.backgroundColor.opacity = 0.2;
+            button.addEventListener("click",switchElement);
+            button.setAttribute("id",elementCatagory[catagories[i]][j]);
+            button.style.display = "none"
+            catagoryButton.appendChild(button);
+        }
+        selector.appendChild(catagoryButton);
+    }
+}
+
+function switchCatagory() {
+    let children = document.getElementById(catagory).children
+    for (let i = 0; i < children.length;i++) {
+        children[i].style.display = "none"
+    }
+    children = this.children
+    for (let i = 0; i < children.length;i++) {
+        children[i].style.display = "block"
+    }
+    catagory = this.id;
+}
+
+function switchElement() {
+    element = this.id;
 }
 
 function displayBoard () {
     context.fillRect(0,0,20,20);
     for (let i = 0; i<boardWidth; i++) {
         for (let j = 0; j < boardHeight; j++) {
-            context.fillStyle= colors[grid[i][j]];
+            context.fillStyle= `rgb(
+                ${colors[grid[i][j]][0]},
+                ${colors[grid[i][j]][1]},
+                ${colors[grid[i][j]][2]})`;
             context.fillRect(i*cellSize,j*cellSize,cellSize,cellSize);
         }
     }
